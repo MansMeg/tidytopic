f8fa1f5fcb4cd0fc7522655c622bd1457ff7c5f6
Måns Magnusson <mons.magnusson@gmail.com>
2016-10-25 17:14:12 -0400

    Added general to do

--- tests/testthat/test-general_todo.R
+++ tests/testthat/test-general_todo.R
@@ -4,5 +4,8 @@ test_that(desc="TODO",{
   skip_on_travis()
   expect_true(FALSE, info = "Fix dplyr an %>% so package work without first loading dplyr")
   expect_true(FALSE, info = "fix write_mallet_state()")
+  expect_true(FALSE, info = "setup Travis")
+  expect_true(FALSE, info = "setup Coverall")
+  expect_true(FALSE, info = "fix github badges")
 })
 

4cbd2c81b2639da388c29c263a371010e733ad01
Måns Magnusson <mons.magnusson@gmail.com>
2016-10-27 15:17:57 -0400

    Fixed data structure tests

--- R/objects.R
+++ R/objects.R
@@ -1,4 +1,4 @@
-#' Assert that \code{x} is a tidy topic state
+#' Assert that \code{x} is a tidy topic state, matrix or array
 #'
 #' @details
 #' The tidy topic state is a \code{tibble} dataset with at least the variables
@@ -24,14 +24,34 @@ assert_state <- function(x){
   is.tidy_topic_state(x)
 }
 
-#' @rdname is.tidy_topic_state
+
+#' Assert that \code{x} is a \code{tidy_topic_matrix} or a \code{tidy_topic_array}
+#'
+#' @description 
+#' The tidy_topic_array/tidy_topic_matrix is a \code{tibble} dataset 
+#' with a \code{"topic"} variable and an integer variable \code{"n"}.
+#' 
+#' The \code{tidy_topic_matrix} do only contain one more variable, 
+#' \code{tidy_topic_matrix} an array can contain multiple dimensions.
+#' 
+#' The datastructure is sparse, meaning 0 counts are not included.
+#'
+#' @param x Object to check if it is a valid \code{tidy_topic_matrix} 
+#' or \code{tidy_topic_array}.
+#'
+#' @export
+is.tidy_topic_array <- function(x){
+  checkmate::assert_class(x, "tbl_df")
+  checkmate::assert_subset(c("n", "topic"), names(x))
+  checkmate::assert_integer(x$n, lower = 1L)
+  TRUE
+}
+
+#' @rdname is.tidy_topic_array
 #' @export
 is.tidy_topic_matrix <- function(x){
-  warning("is.tidy_topic_matrix is not implemented yet")
-  # Should check that tbl_df,
-  # has "topic" and "n"
-  # is sparse (no zeroes)
-
+  is.tidy_topic_array(x)
+  checkmate::assert(ncol(x) == 3)
   TRUE
 }
 

c22e171da553e434ef5cb8befea00bfd93dec5af
Måns Magnusson <mons.magnusson@gmail.com>
2016-10-28 12:46:32 -0400

    Add additional tests

--- tests/testthat/test-assert.R
+++ tests/testthat/test-assert.R
@@ -2,9 +2,24 @@ context("is.tidy_topic_state")
 
 load(system.file("extdata/sotu50.Rdata", package = "tidytopics"))
 
-test_that(desc="imi",{
+test_that(desc="is.tidy_topic_state",{
   state <- dplyr::mutate(sotu50, pos = NULL)
   expect_warning(assert_state(state))
   expect_warning(assert_state_object(state))
   expect_silent(is.tidy_topic_state(state))
 })
+
+context("is.tidy_topic_matrix/array")
+
+test_that(desc="is.tidy_topic_matrix/array",{
+  dtm <- doc_topic_matrix(sotu50)
+  expect_silent(is.tidy_topic_array(dtm))
+  expect_true(is.tidy_topic_array(dtm))
+  
+  expect_silent(is.tidy_topic_matrix(dtm))
+  expect_true(is.tidy_topic_array(dtm))
+
+  expect_false(is.tidy_topic_matrix(dplyr::mutate(dtm, pos = 1:nrow(dtm))))    
+  expect_true(is.tidy_topic_array(dplyr::mutate(dtm, pos = 1:nrow(dtm))))    
+})
+

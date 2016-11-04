4aa4c6017cf0541e40ab3921c3af2f55cfc151fd
Måns Magnusson <mons.magnusson@gmail.com>
2016-11-03 23:35:13 -0400

    Added US presidents documentation

--- R/tidytopics_documentation.R
+++ R/tidytopics_documentation.R
@@ -16,6 +16,23 @@
 #' @useDynLib tidytopics
 NULL
 
+#' US Presidents.
+#'
+#' A dataset containing US presidents from 1789 to 2015
+#'
+#' @format A \code{\link[tibble]{tibble}} \code{data.frame} with 44 rows and 7 variables:
+#' \describe{
+#'   \item{no}{President number.}
+#'   \item{president}{Name of the president.}
+#'   \item{born}{The year the president was born.}
+#'   \item{died}{The year the president died. \code{NA} if alive in 2015.
+#'   \item{presidency_start}{The date the presidency started.}
+#'   \item{presidency_end}{The date the presidency ended.}
+#'   \item{party}{The party of the president.}
+#' }
+#' 
+#' @source \url{https://en.wikipedia.org/wiki/State_of_the_Union}
+"uspresidents"
 
 
 #' State of the Union Adresses.
@@ -25,6 +42,7 @@ NULL
 #' @format A \code{\link[tibble]{tibble}} \code{data.frame} with 6359 rows and 3 variables:
 #' \describe{
 #'   \item{year}{Year of the adress.}
+#'   \item{president}{President that gave the address.}
 #'   \item{paragraph}{The paragraph of the address.}
 #'   \item{text}{The address content.}
 #' }

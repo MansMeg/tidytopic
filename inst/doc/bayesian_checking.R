ee54b88caad426e24ca4131ae749c9fed6d13508
Måns Magnusson <mons.magnusson@gmail.com>
2016-11-08 11:21:44 -0500

    Fixed Bayesian checking vignette

--- inst/doc/bayesian_checking.R
+++ inst/doc/bayesian_checking.R
@@ -1,3 +1,8 @@
+## ---- echo=FALSE---------------------------------------------------------
+# Should the full vignette be run or a fast version.
+# The full version run all code and store the results to run faster later.
+run_full_vignette <- FALSE
+
 ## ------------------------------------------------------------------------
 library(tidytopics)
 library(dplyr)
@@ -21,35 +26,48 @@ observed_imi
 plt <- ggplot_imi_type(observed_imi, topic = 1)
 plt
 
-## ------------------------------------------------------------------------
-plt + theme_bw() + ylab("IMI") + ylim(0, 3.5) + xlab("Word type")
-
-## ------------------------------------------------------------------------
-no_draws <- 25
-replicated_imi <- list()
-replicated_mi <- list()
-for(i in 1:no_draws){
-  sim_tokens <- sample_tokens_given_topics(sotu50)
-  replicated_imi[[i]] <- imi(sim_tokens, w = top_keys)
-  replicated_mi[[i]] <- mi(sim_tokens)
+## ---- echo=FALSE---------------------------------------------------------
+if(run_full_vignette){
+  no_draws <- 25
+  replicated_imi <- list()
+  replicated_mi <- list()
+  for(i in 1:no_draws){
+    sim_tokens <- sample_tokens_given_topics(sotu50)
+    replicated_imi[[i]] <- imi(sim_tokens, w = top_keys)
+    replicated_mi[[i]] <- mi(sim_tokens)
+  }
+  replicated_imi <- bind_rows(replicated_imi)
+  replicated_mi <- bind_rows(replicated_mi)
+  save(replicated_imi, replicated_mi, file = "bayesian_checking_1.Rdata")
+} else {
+  load(file = "bayesian_checking_1.Rdata")
 }
-replicated_imi <- bind_rows(replicated_imi)
-replicated_mi <- bind_rows(replicated_mi)
+
+
+## ---- eval=FALSE---------------------------------------------------------
+#  no_draws <- 25
+#  replicated_imi <- list()
+#  replicated_mi <- list()
+#  for(i in 1:no_draws){
+#    sim_tokens <- sample_tokens_given_topics(sotu50)
+#    replicated_imi[[i]] <- imi(sim_tokens, w = top_keys)
+#    replicated_mi[[i]] <- mi(sim_tokens)
+#  }
+#  replicated_imi <- bind_rows(replicated_imi)
+#  replicated_mi <- bind_rows(replicated_mi)
 
 ## ------------------------------------------------------------------------
-observed_imi
 plt <- ggplot_imi_type(observed_imi, replicated_imi, topic = 1)
 plt
 
 ## ------------------------------------------------------------------------
 ggplot(data = filter(replicated_mi, topic == 1), aes(x = mi)) +
   geom_histogram(bins = 100) + 
-  xlim(c(3,5)) + 
-  geom_vline(xintercept =filter(observed_mi, topic == 1)[["mi"]])
+  geom_vline(xintercept = filter(observed_mi, topic == 1)[["mi"]])
 
 ## ------------------------------------------------------------------------
 mi_dev <- mi_deviance(observed_mi, replicated_mi)
-ggplot(data = mi_dev, aes(x = deviance)) + geom_histogram(bins = 30)
+mi_dev
 
 ## ------------------------------------------------------------------------
 data(sotu)
@@ -64,27 +82,54 @@ observed_mi_random_group <- mi(sotu50, g = "random_group")
 observed_imi_president <- imi(sotu50, g = "president", w = top_keys)
 observed_imi_random_group <- imi(sotu50, g = "random_group", w = top_keys)
 
-## ------------------------------------------------------------------------
-no_draws <- 25
-replicated_mi_president <- list()
-replicated_mi_random_group <- list()
-replicated_imi_president <- list()
-replicated_imi_random_group <- list()
-for(i in 1:no_draws){
-  sim_tokens <- sample_tokens_given_topics(sotu50)
-  replicated_mi_president[[i]] <- 
-    mi(sim_tokens, g = "president")
-  replicated_imi_president[[i]] <- 
-    imi(sim_tokens, g = "president", w = top_keys)
-  replicated_mi_random_group[[i]] <- 
-    mi(sim_tokens, g = "president")
-  replicated_imi_random_group[[i]] <- 
-    imi(sim_tokens, g = "president", w = top_keys)
+## ---- echo=FALSE---------------------------------------------------------
+if(run_full_vignette){
+  no_draws <- 25
+  replicated_mi_president <- list()
+  replicated_mi_random_group <- list()
+  replicated_imi_president <- list()
+  replicated_imi_random_group <- list()
+  for(i in 1:no_draws){
+    sim_tokens <- sample_tokens_given_topics(sotu50)
+    replicated_mi_president[[i]] <- 
+      mi(sim_tokens, g = "president")
+    replicated_imi_president[[i]] <- 
+      imi(sim_tokens, g = "president", w = top_keys)
+    replicated_mi_random_group[[i]] <- 
+      mi(sim_tokens, g = "president")
+    replicated_imi_random_group[[i]] <- 
+      imi(sim_tokens, g = "president", w = top_keys)
+  }
+  replicated_mi_president <- bind_rows(replicated_mi_president)
+  replicated_mi_random_group <- bind_rows(replicated_mi_president)
+  replicated_imi_president <- bind_rows(replicated_imi_president)
+  replicated_imi_random_group <- bind_rows(replicated_imi_random_group)
+  save(replicated_mi_president, replicated_mi_random_group, replicated_imi_president, replicated_imi_random_group, file = "bayesian_checking_2.Rdata")
+} else {
+  load("bayesian_checking_2.Rdata")
 }
-replicated_mi_president <- bind_rows(replicated_mi_president)
-replicated_mi_random_group <- bind_rows(replicated_mi_president)
-replicated_imi_president <- bind_rows(replicated_imi_president)
-replicated_imi_random_group <- bind_rows(replicated_imi_random_group)
+
+## ---- eval=FALSE---------------------------------------------------------
+#  no_draws <- 25
+#  replicated_mi_president <- list()
+#  replicated_mi_random_group <- list()
+#  replicated_imi_president <- list()
+#  replicated_imi_random_group <- list()
+#  for(i in 1:no_draws){
+#    sim_tokens <- sample_tokens_given_topics(sotu50)
+#    replicated_mi_president[[i]] <-
+#      mi(sim_tokens, g = "president")
+#    replicated_imi_president[[i]] <-
+#      imi(sim_tokens, g = "president", w = top_keys)
+#    replicated_mi_random_group[[i]] <-
+#      mi(sim_tokens, g = "president")
+#    replicated_imi_random_group[[i]] <-
+#      imi(sim_tokens, g = "president", w = top_keys)
+#  }
+#  replicated_mi_president <- bind_rows(replicated_mi_president)
+#  replicated_mi_random_group <- bind_rows(replicated_mi_president)
+#  replicated_imi_president <- bind_rows(replicated_imi_president)
+#  replicated_imi_random_group <- bind_rows(replicated_imi_random_group)
 
 ## ------------------------------------------------------------------------
 mi_dev_pres <- 
